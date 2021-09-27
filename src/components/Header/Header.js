ba9b47c7a0688b3492be5b2aa3f541625c324648
Дмитрий Верзаков <dverzakov@studio-tg.ru>
2021-09-27 10:27:50 +0300

    Resolve "2. Компонент отображения заголовка страницы"

---
+++ src/components/Header/Header.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import {Text, View, TouchableOpacity, Image} from 'react-native';
+import {styles} from './HeaderStyles';
+
+const Header = props => {
+  const {title, onPressLeft, onPressRight} = props;
+
+  const renderButtonBack = () => {
+    if (onPressLeft) {
+      return (
+        <TouchableOpacity
+          onPress={onPressLeft}
+          style={styles.buttonContainerBack}>
+          <Image
+            style={styles.imageStyleBack}
+            source={require('../../images/nextAndBack/forwardBlue.png')}
+            tintColor="blue"
+          />
+          <Text style={styles.buttonText}>left</Text>
+        </TouchableOpacity>
+      );
+    } else {
+      return <View style={styles.buttonContainerBack} />;
+    }
+  };
+
+  const renderButtonNext = () => {
+    if (onPressRight) {
+      return (
+        <TouchableOpacity
+          onPress={onPressRight}
+          style={styles.buttonContainerNext}>
+          <Text style={styles.buttonText}>right</Text>
+          <Image
+            style={styles.imageStyleNext}
+            source={require('../../images/nextAndBack/forwardBlue.png')}
+            tintColor="blue"
+          />
+        </TouchableOpacity>
+      );
+    } else {
+      return <View style={styles.buttonContainerNext} />;
+    }
+  };
+  return (
+    <View style={styles.container}>
+      {renderButtonBack()}
+      <View style={styles.titleContainer}>
+        <Text style={styles.text}>{title}</Text>
+      </View>
+      {renderButtonNext()}
+    </View>
+  );
+};
+
+export default Header;
