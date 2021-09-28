e62e93a81c6936cae606b335427147f40811dffb
Дмитрий Верзаков <dverzakov@studio-tg.ru>
2021-09-28 11:24:39 +0300

    Resolve "3. Загрузка данные для списка пользователей"

---
+++ src/modules/UsersList/UsersListStyles.js
@@ -0,0 +1,17 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  containerList: {
+    flexGrow: 1,
+  },
+  container: {
+    flex: 1,
+    position: 'absolute',
+    top: -30.28,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
